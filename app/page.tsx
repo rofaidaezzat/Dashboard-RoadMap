d44dbbfc4a8e7a89d6752f5695b9f453db0f0b90
rofaidaezzat <rofaidaezzat@gmail.com>
2025-06-13 19:58:54 +0300

    remve login

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,33 @@
 "use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
 import Loading from "./components/Ui/Loading/Loading";
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 
 export default function Home() {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const checkUserData = () => {
-      try {
-        const userDataString = Cookies.get("loggedInUser");
-        const userData = userDataString ? JSON.parse(userDataString) : null;
-        const roleUser = Cookies.get("role");
-        const roleData = roleUser ? JSON.parse(roleUser) === "admin" : null;
+  // useEffect(() => {
+  //   const checkUserData = () => {
+  //     try {
+  //       const userDataString = Cookies.get("loggedInUser");
+  //       const userData = userDataString ? JSON.parse(userDataString) : null;
+  //       const roleUser = Cookies.get("role");
+  //       const roleData = roleUser ? JSON.parse(roleUser) === "admin" : null;
 
-        if (userData && roleData) {
-          router.push("/pages/pagesofsidebar/Home");
-        } else {
-          router.push("/authpage");
-        }
-      } catch (error) {
-        console.error("Error accessing cookies:", error);
-        router.push("/authpage");
-      }
-    };
+  //       if (userData && roleData) {
+  //         router.push("/pages/pagesofsidebar/Home");
+  //       } else {
+  //         router.push("/authpage");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error accessing cookies:", error);
+  //       router.push("/authpage");
+  //     }
+  //   };
 
-    checkUserData();
-  }, [router]);
+  //   checkUserData();
+  // }, [router]);
 
   return (
     <div className="min-h-screen flex justify-center items-center w-full p-4">
